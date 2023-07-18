ec9995f6f94c342b1d85f2380e407623e763c52a
KimJammer <41841812+kimjammer@users.noreply.github.com>
2023-07-18 18:48:32 -0400

    Make Hero section responsive.

--- src/components/ContactChip.svelte
+++ src/components/ContactChip.svelte
@@ -11,9 +11,19 @@
 <style>
 	.container {
 		border-radius: 0.5em;
-		width: 3em;
-		height: 3em;
+		width: 2em;
+		height: 2em;
 		background-color: #EDF5FF7F;
 		border: #596be3 1px solid;
+		transition: 0.5s ease;
+	}
+	@media (min-width:430px) {
+		.container {
+			width: 3em;
+			height: 3em;
+		}
+	}
+	.container:hover {
+		box-shadow: 4px 4px 13px  rgba(0,0,0,0.75);
 	}
 </style>
--- src/components/HeroSection.svelte
+++ src/components/HeroSection.svelte
@@ -11,28 +11,30 @@ import ContactChip from "./ContactChip.svelte";
 					<h1>KimJammer</h1>
 					<h1>John Kim</h1>
 				</div>
-				<img src="/Logo.svg" alt="KimJammer Logo">
-			</div>
-
-			<div class="contacts">
-				<ContactChip link="https://github.com/kimjammer">
-					<img class="contactLogo" src="/github.svg" alt="Github">
-				</ContactChip>
-				<ContactChip link="https://discord.com/users/424546246980665344">
-					<img class="contactLogo" src="/discord.svg" alt="Discord">
-				</ContactChip>
-				<ContactChip link="https://twitter.com/kimjammer1">
-					<img class="contactLogo" src="/twitter.svg" alt="Twitter">
-				</ContactChip>
-				<ContactChip link="https://mastodon.social/@kimjammer">
-					<img class="contactLogo" src="/mastodon.svg" alt="Mastodon">
-				</ContactChip>
-				<ContactChip link="mailto:contact@kimjammer.com">
-					<img class="contactLogo" src="/email.svg" alt="Email">
-				</ContactChip>
+				<img src="/icons/Logo.svg" alt="KimJammer Logo">
 			</div>
 
 			<p>Developer. Student. Designer.</p>
+
+			<div class="contacts">
+				<ContactChip link="https://github.com/kimjammer">
+					<img class="contactLogo" src="/icons/github.svg" alt="Github">
+				</ContactChip>
+				<ContactChip link="https://discord.com/users/424546246980665344">
+					<img class="contactLogo" src="/icons/discord.svg" alt="Discord">
+				</ContactChip>
+				<ContactChip link="https://twitter.com/kimjammer1">
+					<img class="contactLogo" src="/icons/twitter.svg" alt="Twitter">
+				</ContactChip>
+				<ContactChip link="https://mastodon.social/@kimjammer">
+					<img class="contactLogo" src="/icons/mastodon.svg" alt="Mastodon">
+				</ContactChip>
+				<ContactChip link="mailto:contact@kimjammer.com">
+					<img class="contactLogo" src="/icons/email.svg" alt="Email">
+				</ContactChip>
+			</div>
+
+
 		</div>
 	</div>
 </div>
@@ -46,21 +48,8 @@ import ContactChip from "./ContactChip.svelte";
 		width: 100%;
 		height: 100vh;
 	}
-	.contacts{
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: start;
-		gap: 1em;
-		align-items: center;
-	}
-	.contactLogo {
-		width: 70%;
-		position: relative;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-	}
 	.card{
+		position: relative;
 		width: 70vw;
 		height: 50vh;
 		background-color: #EDF5FF7F;
@@ -74,11 +63,16 @@ import ContactChip from "./ContactChip.svelte";
 		animation: fadeIn 1s ease-in-out forwards;
 
 		transition: 0.5s ease;
-		box-shadow: 0px 0px 10px 0px #596be3;
+		box-shadow: 0 0 10px 0 #596be3;
 	}
 	.card:hover{
 		scale: 1.015;
-		box-shadow: 4px 4px 13px 0px #596be3;
+		box-shadow: 4px 4px 13px 0 #596be3;
+	}
+	@media (max-height: 890px) {
+		.card {
+			height: 70vh;
+		}
 	}
 	@keyframes fadeIn {
 		0% {
@@ -89,19 +83,54 @@ import ContactChip from "./ContactChip.svelte";
 		}
 	}
 
+	.contacts{
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: start;
+		gap: 1em;
+		align-items: center;
+		position: absolute;
+		bottom: 2em;
+		margin-right: 2em;
+	}
+	@media (min-width:430px) {
+		.contacts{
+			gap: 1.5em;
+		}
+	}
+	.contactLogo {
+		width: 70%;
+		position: relative;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+
 	.header{
 		display: flex;
 		justify-content: space-between;
 		align-items: flex-start;
+		flex-direction: column-reverse;
+		gap: 1em;
 	}
+	@media (min-width:430px) {
+		.header {
+			flex-direction: row;
+		}
+	}
+
 	img{
 		width: 20vmin;
 	}
 	h1 {
+		margin: 0;
 		font-size: 7vmin;
-		margin-top: 0.2em;
+		line-height: 8.4vmin;
+		font-weight: 700;
 	}
 	p {
-		font-size: 4vmin;
+		margin: 0;
+		font-size: 5vmin;
+		line-height: 7.5vmin;
 	}
 </style>
